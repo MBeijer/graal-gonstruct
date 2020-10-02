b0cc5ca1a0f23ac68bef997e582016ef1bfe5f3b
Marlon Beijer <marlon@eevul.org>
2020-10-02 21:44:03 +0200

    Fix win32 building

--- build-win32.sh
+++ build-win32.sh
@@ -3,5 +3,5 @@ export TOOLCHAIN="i686-w64-mingw32.shared"
 export PREFIX="/usr/src/mxe/usr/${TOOLCHAIN}"
 ./prepare/mingw-w32.sh
 # shellcheck disable=SC2046
-cmake -S. -Bbuild-win && cmake --build build-win32 -- -j $(nproc)
+cmake -S. -Bbuild-win32 && cmake --build build-win32 -- -j $(nproc)
 MINGW_BUNDLEDLLS_SEARCH_PATH="${PREFIX}/bin" mingw-bundledlls --copy bin/gonstruct.exe
